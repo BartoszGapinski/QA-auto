d7c0ab8db390bff29f2eeb6fc703baf85856b6da
BartoszGapinski <gapinskibartosz@gmail.com>
2023-02-06 21:29:02 +0100

    Dodana funkcjonalnosc infomracje o paczce

--- script.js
+++ script.js
@@ -7,22 +7,52 @@ function myColorChange() {
     const element = document.getElementById("id01");
     element.style.color = "red";
 }
-function mojaFunkcja(imie) {
 
-    const clients = ["Mango", "Poly", "Ajax"];
-    const clientNameToFind = "Patryk";
-    let message;
-    
-    for (const client of clients) {
-    
-      if (client === imie) {
-        message = "Klient z takim imieniem jest w bazie danych!";
-        break;
-      }
-    
-      message = "Nie znaleźliśmy takiego klienta w bazie danych!";
-    }
-    
-    console.log(message);
+const przesylka = {
+  name: "",
+  ulica: "",
+};
+
+const przesylka2 = {
+  name: "",
+  ulica: "",
+};
+
+przesylka2.waga = 20;
+przesylka.name = "List";
+przesylka2.name = "Paczka";
+
+
+
+function mojaFunkcja3(id) {
+  const element = document.getElementById("id01");
+  const element2 = document.getElementById("id02");
+
+if (id=== 1 ){
+  informacje = "Typ twojej przesyłki to: " + przesylka.name
+  element.innerHTML = informacje;
+  
+  informacje = "przesylka nie posiada wagi"
+  element2.innerHTML = informacje;
+}
+
+if (id === 2){
+  informacje2 = "Typ twojej przesyłki to: " + przesylka2.name
+  element.innerHTML = informacje2;
+  
+  informacje2 = "waga twojej przesyłki to: " + przesylka2.waga + "kg"
+  element2.innerHTML = informacje2;
+}
+  
+  
+
+
+
+  
+ 
     
 }
+
+
+
+
